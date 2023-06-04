7a58ad13b085cb6d7bf4ca48fe5c412ae2ec8a4f
MinGi-K <mingikwon1106@gmail.com>
2023-06-05 07:51:54 +0800

    refactored navbar to its own component away from Home:

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import Home from "./components/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import './Home.css';
-import {ReactComponent as UniSpaceLogo} from '../../assets/UnispaceLogo.svg';
-import {ReactComponent as WriteSymbol} from '../../assets/WriteSymbol.svg';
+import { ReactComponent as UniSpaceLogo } from '../../assets/UnispaceLogo.svg';
+import { ReactComponent as WriteSymbol } from '../../assets/WriteSymbol.svg';
+import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
-        
-        <div className="container">
-            <UniSpaceLogo className = "logo"/>
-            <nav>
-                <ul>
-                    <li><WriteSymbol className = "writeSymbol"/></li>
-                    <li><a href="#">Write</a></li>
-                    <li><a href="#">|</a></li>
-                    <li><a href="#">Sign In</a></li>
-                    <li><a href="#">Get Started</a></li>
-                </ul>
-            </nav>
-            
-        </div>
+        <>
+            <a> Work in progress </a>
+        </>
     );
 }
 
