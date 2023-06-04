a77d4731d490f1b7dc992775008434bdff850807
MinGi-K <mingikwon1106@gmail.com>
2023-06-04 22:35:02 +0800

    added Inter font

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
-    return(
-        <div>
-            Hello World
+    return (
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
         </div>
     );
 }
