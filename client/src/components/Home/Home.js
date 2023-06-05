b1d967eb38fa27a4a00716ef222fb27d30456fb7
MinGi-K <mingikwon1106@gmail.com>
2023-06-05 15:09:41 +0800

    added basic structure for content for the page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,9 +6,21 @@ import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     return (
-        <>
-            <a> Work in progress </a>
-        </>
+        <div className= "container" >
+            <div className="gridContainer">
+                
+                <div className='col popular'>
+                    <a>Popular goes here</a>
+                </div>
+                
+                <div className='col todayPicks'>
+                    <a> Today's picks goes here, make another grid</a>
+                </div>
+            </div>
+            <div className='content container'>
+                <a> articles come here, make another grid for the articles </a>
+            </div>
+        </div>
     );
 }
 
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.css';
+import {ReactComponent as UniSpaceLogo} from '../../assets/UnispaceLogo.svg';
+import {ReactComponent as WriteSymbol} from '../../assets/WriteSymbol.svg';
+
+const Navbar = () => {
+    return (
+        
+        <div className="navbar container">
+            <UniSpaceLogo className = "logo"/>
+            <nav>
+                <ul>
+                    <li><WriteSymbol className = "writeSymbol"/></li>
+                    <li><a href="#">Write</a></li>
+                    <li><a href="#">|</a></li>
+                    <li><a href="#">Sign In</a></li>
+                    <li><a href="#">Get Started</a></li>
+                </ul>
+            </nav>
+            
+        </div>
+    );
+}
+
+export default Navbar;
