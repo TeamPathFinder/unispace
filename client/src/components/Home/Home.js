7ebd20cd707b38000bbf7bba00c115123c09d9a6
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-06-10 23:50:18 +0900

    added title / empty background

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,18 +5,25 @@ import { ReactComponent as WriteSymbol } from '../../assets/WriteSymbol.svg';
 import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
+
     return (
         <div className="container gradient">
             <div className="flex slogan">
-                <h2>
-                    Connect to our space, <a>unispace </a>
-                </h2>
+                <h4>
+                    Connect to our space, <a href="#">unispace</a>
+                </h4>
             </div>
             <div className="gridContainer">
-                <div className="col todayPicks">
-                    <a> Today's picks goes here</a>
+                <div className="col todaysPick">
+                    <h2>Today's Pick</h2>
+                    <a href="#" style={{ backgroundImage: ''}}>
+                        <div className="featuredArticle">
+                            <h3>Featured Article Title</h3>
+                        </div>
+                    </a>
                 </div>
                 <div className="col popular">
+                    <h2>What's <span className="highlight">Popular</span> This Week</h2>
                     <a>Popular goes here, make another grid</a>
                 </div>
             </div>
