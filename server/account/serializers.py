c34fecb39fa1be269603b2fb1fdff20125333c48
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2023-06-26 00:48:20 +0900

    Add PopularContents API

--- server/account/serializers.py
+++ server/account/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework import serializers
 from .models import User
 
+
 class RegisterUserSerializer(serializers.ModelSerializer):
     # Write only means that the field will not be returned in the response
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'name', 'password', 'nickname', 'school', 'country', 'major']
+        fields = ["email", "name", "password", "nickname", "school", "country", "major"]
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
+        password = validated_data.pop("password")
         user = User.objects.create_user(**validated_data)
         user.set_password(password)
         user.save()
         return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "name", "nickname", "school", "country", "major"]
