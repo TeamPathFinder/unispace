847374e12adf1745899f4da8829d8233db5c8325
MinGi-K <mingikwon1106@gmail.com>
2023-07-29 18:26:34 +0800

    added bottom buttons

--- client/src/components/Interview/Interview.js
+++ client/src/components/Interview/Interview.js
@@ -1,15 +1,34 @@
 import React from "react"
 import "./Interview.css"
 import QuestionAnswer from "./subcomponents/QuestionAnswer"
+import { ReactComponent as Coffee } from '../../assets/coffee.svg';
+import { ReactComponent as Paperclip } from '../../assets/paperclip.svg';
+
+
 
 const Interview = () => {
-    return(
+    return (
         <div className="interviewContainer">
             <div className="questionsContainer">
-                <QuestionAnswer isHighlighted={true}/>
-                <QuestionAnswer isHighlighted={false}/>
+                <QuestionAnswer isHighlighted={true} />
+                <QuestionAnswer isHighlighted={false} />
+                <div className="flex bottomButtons">
+                    <div className="blueButton">
+                        <div style={{ margin: 0, flexDirection: 'row', display: 'flex', alignItems: 'center' }}>
+                            <Coffee style={{ height: '16px', display: 'flex', marginRight: '1vw' }}></Coffee>
+                            <a> 커피챗 요청하기 </a>
+                        </div>
+                    </div>
+
+                    <div className="blueButton">
+                        <div style={{ margin: 0, flexDirection: 'row', display: 'flex', alignItems: 'center' }}>
+                            <Paperclip style={{ width: '20px', display: 'flex', marginRight: '1vw' }}></Paperclip>
+                            <a> 공유하기 </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
+
         </div>
     )
 
