0a961958956bc9526d62fb40faaa988c09c1b793
bigsmile <ryubsmile@gmail.com>
2023-07-29 14:22:34 +0900

    Revert "feature: update header"

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import Home from "./components/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Interview from "./components/Interview/Interview";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/interviews" element={<Interview/>}/>
       </Routes>
     </>
   );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import './Navbar.css';
-import { ReactComponent as UniSpaceLogo } from '../../assets/UnispaceLogo.svg';
-import { Link } from 'react-router-dom';
-import { LinkContainer } from 'react-router-bootstrap';
+import {ReactComponent as UniSpaceLogo} from '../../assets/UnispaceLogo.svg';
+import {ReactComponent as WriteSymbol} from '../../assets/WriteSymbol.svg';
 
 const Navbar = () => {
     return (
-
+        
         <div className="navbar container">
-            <LinkContainer to="/" style={{cursor: "pointer"}}>
-                <UniSpaceLogo className="logo" />
-            </LinkContainer>
+            <UniSpaceLogo className = "logo"/>
             <nav>
                 <ul>
-                    <li><Link to="/interviews"><a>커피챗</a></Link></li>
-                    <li><Link><a>리소스</a></Link></li>
-                    <li><Link><a>팀빌딩</a></Link></li>
-                    <li><Link><a>챌린지</a></Link></li>
-                    <li><Link><a>블로그</a></Link></li>
+                    <li><WriteSymbol className = "writeSymbol"/></li>
+                    <li><a href="#">Sign In</a></li>
+                    <li><a href="#">Get Started</a></li>
                 </ul>
             </nav>
-
-            <div className="startButton">
-                <a> 시작하기 </a>
-            </div>
-
+            
         </div>
     );
 }
