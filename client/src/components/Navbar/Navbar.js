4170c92ce01f69755b1a6b8d588e6448da6d1a51
Jaehyuk Ryu <ryubsmile@gmail.com>
2023-08-05 18:09:25 +0900

    navbar update

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './Navbar.css';
 import { ReactComponent as UniSpaceLogo } from '../../assets/UnispaceLogo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Navbar = () => {
+    const topLocation = useLocation().pathname.split('/')[1];
+    console.log(topLocation);
+    const activateLink = location => location === topLocation ? 'active' : '';
+
     return (
 
         <div className="navbar container">
-            <LinkContainer to="/" style={{cursor: "pointer"}}>
+            <LinkContainer to="/" style={{ cursor: "pointer" }}>
                 <UniSpaceLogo className="logo" />
             </LinkContainer>
             <nav>
                 <ul>
-                    <li><Link to="/interviews"><a>커피챗</a></Link></li>
+                    <li className={activateLink('interviews')}><Link to="/interviews"><a>커피챗</a></Link></li>
                     <li><Link><a>리소스</a></Link></li>
                     <li><Link><a>팀빌딩</a></Link></li>
                     <li><Link><a>챌린지</a></Link></li>
-                    <li><Link><a>블로그</a></Link></li>
+                    <li className={activateLink('blog')}><Link to="/blog"><a>블로그</a></Link></li>
                 </ul>
             </nav>
 
