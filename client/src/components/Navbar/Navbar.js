51c8b797b3569715a1741ed86ab3c4f4fb26a7cf
MinGi-K <mingikwon1106@gmail.com>
2023-08-06 00:16:00 +0800

    changed navbar to new design

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,30 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { ReactComponent as UniSpaceLogo } from '../../assets/UnispaceLogo.svg';
 import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Navbar = () => {
+
+    const [selectedCat, setSelectedCat] = useState([0, 0, 0, 0, 0])
+
+    const handle_change_cat = (i) => {
+        const temp = [0, 0, 0, 0, 0];
+        temp[i] = 1;
+        setSelectedCat(temp);
+    }
+
+    const cat_style = ["flex nav-category", "flex nav-category-selected"]
+
     return (
 
-        <div className="navbar container">
-            <LinkContainer to="/" style={{cursor: "pointer"}}>
-                <UniSpaceLogo className="logo" />
-            </LinkContainer>
-            <nav>
-                <ul>
-                    <li><Link to="/interviews"><a>커피챗</a></Link></li>
-                    <li><Link><a>리소스</a></Link></li>
-                    <li><Link><a>팀빌딩</a></Link></li>
-                    <li><Link><a>챌린지</a></Link></li>
-                    <li><Link><a>블로그</a></Link></li>
-                </ul>
-            </nav>
+        <div className='navbar-underline'>
+            <div className="navbar-container">
+                <LinkContainer to="/" style={{ cursor: "pointer" }}>
+                    <UniSpaceLogo className="logo" />
+                </LinkContainer>
+                <div className='flex fd-row nav-category-container'>
 
-            <div className="startButton">
-                <a> 시작하기 </a>
+                    <LinkContainer
+                        to="/interviews"
+                        className={cat_style[selectedCat[0]]}
+                        onClick={() => handle_change_cat(0)}
+                    ><a>커피챗</a></LinkContainer>
+                    <LinkContainer
+                        to="/"
+                        className={cat_style[selectedCat[1]]}
+                        onClick={() => handle_change_cat(1)}
+                    ><a>리소스</a></LinkContainer>
+                    <LinkContainer
+                        to="/"
+                        className={cat_style[selectedCat[2]]}
+                        onClick={() => handle_change_cat(2)}
+                    ><a>팀빌딩</a></LinkContainer>
+                    <LinkContainer
+                        to="/"
+                        className={cat_style[selectedCat[3]]}
+                        onClick={() => handle_change_cat(3)}
+                    ><a>챌린지</a></LinkContainer>
+                    <LinkContainer
+                        to="/"
+                        className={cat_style[selectedCat[4]]}
+                        onClick={() => handle_change_cat(4)}><a>블로그</a></LinkContainer>
+
+                </div>
+
+                <div className="startButton">
+                    <a> 시작하기 </a>
+                </div>
+
             </div>
-
         </div>
     );
 }
