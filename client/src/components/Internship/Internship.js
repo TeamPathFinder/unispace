6de86568527be674ca9734e64acd63876b4eaaff
MinGi-K <mingikwon1106@gmail.com>
2023-08-15 00:59:42 +0800

    squash

---
+++ client/src/components/Internship/Internship.js
@@ -0,0 +1,18 @@
+import './Internship.css'
+
+
+const Internship = () => {
+    
+    return(
+        <div className="internship-container flex fd-col">
+            <div className="flex internship-content-container">
+                <div className="internship-header-container">
+                    <h2> 인턴쉽 </h2>
+                    <a> 다가오는 2024 하계 인턴십을 위한 space </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Internship;
