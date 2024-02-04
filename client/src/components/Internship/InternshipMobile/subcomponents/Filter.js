2d65eadf2953d08bc554f48550850e39af8f1c71
mingikwon <mingikwon1106@gmail.com>
2024-02-04 03:07:57 -0500

    added filter

--- client/src/components/Internship/InternshipMobile/subcomponents/Filter.js
+++ client/src/components/Internship/InternshipMobile/subcomponents/Filter.js
@@ -1,11 +1,60 @@
 import React from "react";
 import '../InternshipMobile.css';
+import { ReactComponent as FilterIcon } from "../../../../assets/filter.svg";
 
-const Filter = () => {
+import { ReactComponent as CollapsibleArrow } from "../../../../assets/collapsible_arrow.svg";
+
+import FilterOption from "../../subcomponents/FilterOption";
+const Filter = ({ filterOptions, renderFilterList, handleCollapsibleHeader, collapsibleHeaders, setIsFilterFocus, isFilterFocus }) => {
+
     return (
-        <div>
-            FilterOption
-        </div>
+        <>
+            <div className="mobile-filter-container">
+                <div className="mobile-filter-section fd-row">
+                    <FilterIcon className="filter-icon" style={{ height: '18px' }} />
+                    <p className="mobile-filter-header" style={{ marginLeft: '8px' }}>Filter</p>
+                </div>
+
+                <div className="mobile-filter-section fd-row align-center"
+                    onClick={(e) => handleCollapsibleHeader(e, "countries")}
+                    style={{ justifyContent: 'space-between' }}
+                >
+                    <p className="mobile-filter-header"> Select Country </p>
+                    <CollapsibleArrow
+                        className={`filter-collapsible-arrow-mobile ${collapsibleHeaders["countries"] ? "flip-vertical" : ""
+                            }`}
+                    />
+                </div>
+
+                <div className={`mobile-filter-section fd-col ${collapsibleHeaders["countries"] ? "" : "closed"
+                    }`} >
+                    {renderFilterList(12, 16)}
+                </div>
+
+                <div className="mobile-filter-section fd-row align-center"
+                    onClick={(e) => handleCollapsibleHeader(e, "cities")}
+                    style={{ justifyContent: 'space-between' }}
+                >
+                    <p className="mobile-filter-header"> Select Location </p>
+                    <CollapsibleArrow
+                        className={`filter-collapsible-arrow-mobile ${collapsibleHeaders["cities"] ? "flip-vertical" : ""
+                            }`}
+                    />
+                </div>
+
+                <div className={`mobile-filter-section fd-col ${collapsibleHeaders["cities"] ? "" : "closed"
+                    }`} >
+                    {renderFilterList(0, 11)}
+                </div>
+            </div>
+
+            <button className="mobile-filter-apply-button" onClick={()=>{
+                console.log("this shouldn't be happening")
+                // setIsFilterFocus(false)
+                }}>
+                Apply
+            </button>
+        </>
     )
 }
 
