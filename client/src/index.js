682252cc4de3ccc1baa98ca5f206c5d923af6151
Ryan Moon <74520918+ryan-kanghyun-moon@users.noreply.github.com>
2024-02-05 12:46:19 -0800

    mute console.log for production and change website description

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
+// from https://stackoverflow.com/questions/56276325/how-can-remove-console-log-in-the-production-build-of-a-react-application-create
+// replace console.* for disable log on production
+if (process.env.REACT_APP_NODE_ENV === 'production') {
+  console.log = () => {}
+  console.error = () => {}
+  console.debug = () => {}
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
